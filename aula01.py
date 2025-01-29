4e7ca96165b3cbf119a0b321059e5483eb8756d0
Weslley Costa <weslleycosta.ti@gmail.com>
2025-01-29 20:39:42 -0300

    primeiro commit

---
+++ aula01.py
@@ -0,0 +1,20 @@
+# numA = 18+12+8j 
+
+
+# a = 2
+# b = '2'
+
+# print(type(numA))
+# print(type(a))
+# print(type(b))
+
+
+
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+altura = float(input('Digite sua altura: '))
+
+print(nome ,'-', type(nome))
+print(idade ,'-', type(idade))
+print(altura ,'-', type(altura))
+
---
+++ aula03.py
@@ -0,0 +1,2 @@
+a = input('Digite um texto')
+print(a)
